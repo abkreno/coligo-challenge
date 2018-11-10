436dacf427dd35ee613cf4499e74c4735ba554eb
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-10 13:09:30 +0200

    Add mode to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./index.js",
+  mode: process.env.NODE_ENV || 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
