fb070b43d32c1311c8868d22a5f1f2bc58a62f2f
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-10 13:05:10 +0200

    Add webpack plugin for html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./index.js",
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
   },
-  context: path.resolve(__dirname, 'src')
-}
+  context: path.resolve(__dirname, 'src'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    })
+  ],
+};
