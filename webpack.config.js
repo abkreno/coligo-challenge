520d8e7084728e3d7adfc5d388b1c84d33c21f4e
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-10 13:12:35 +0200

    Add webpack-dev-server/ Add npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public/assets'),
+    stats: 'errors-only',
+    open: true, // to open the browser tab automatically
+    port: 8080,
+    compress: true,
+  },
   context: path.resolve(__dirname, 'src'),
   plugins: [
     new HtmlWebpackPlugin({
