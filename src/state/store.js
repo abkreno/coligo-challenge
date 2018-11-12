402ac87146f4cc05ff26339127b79b58d5ac1711
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-12 21:15:28 +0200

    Add devtools extension

--- src/state/store.js
+++ src/state/store.js
@@ -6,5 +6,6 @@ export default function configureStore(initialState = {}) {
   return createStore(
     rootReducer,
     initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 }
