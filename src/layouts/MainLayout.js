4bdf9d3600e271b1731ad9d554bece3e3010682f
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-17 17:41:14 +0200

    Use main header/sider

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,13 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withAuth from '../withAuth';
 import './MainLayout.scss';
-import Sider from '../components/Sider';
-import DropDown from '../components/DropDown';
-import Menu from '../components/Menu';
-import MenuLink from '../components/MenuLink';
-import defaultAvatar from '../assets/img/avatar.jpg';
-
-const getSiderLinks = isLoggedIn => (isLoggedIn ? [{ text: 'Dashboard', icon: 'dashboard' }] : [{ text: 'Home', icon: 'home' }]);
+import MainHeader from './MainHeader';
+import MainSider from './MainSider';
 
 const MainLayout = ({
   children,
@@ -16,34 +11,17 @@ const MainLayout = ({
   login,
   logout,
 }) => (
-  <div className="layout layout-has-sider">
-    <div className="layout-sider">
-      <Sider>
-        <Menu>
-          {getSiderLinks(isLoggedIn).map((link, idx) => <MenuLink key={idx} {...link} />)}
-        </Menu>
-      </Sider>
-    </div>
-    <div className="layout-content">
-      <div className="layout-header">
-        <div className="layout-header-left">
-          {isLoggedIn && <h1>Welcom Talia,</h1>}
-        </div>
-        <div className="layout-header-right">
-          {!isLoggedIn && <button onClick={login}>Login</button>}
-          {isLoggedIn &&
-          <DropDown
-            menu={(
-              <Menu>
-                {[<MenuLink key={0} onClick={logout} text="Logout" icon="logout" />]}
-              </Menu>)}
-          >
-            <img src={defaultAvatar} alt="Default Avatar" />
-          </DropDown>}
-        </div>
+  <div className="layout-container">
+    <div className="layout">
+      <div className="layout-sider-container">
+        <MainSider isLoggedIn={isLoggedIn} />
       </div>
-      <h2>Welcome to Coligo <small>({isLoggedIn ? 'Logged In' : 'Logged Out'})</small></h2>
-      {children}
+      <div className="layout-header-container">
+        <MainHeader isLoggedIn={isLoggedIn} login={login} logout={logout} />
+      </div>
+      <div className="layout-content-container">
+        {children}
+      </div>
     </div>
   </div>
 );
