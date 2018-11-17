9ec0f49e54bb8dba2813a3682c7035703647c942
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-17 17:41:38 +0200

    Add grid styling for mainlayout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withAuth from '../withAuth';
 
-const Home = ({ login }) => (
+const Home = ({ login, isLoggedIn }) => (
   <div>
     <div>
       <h3>Home Page</h3>
       <button onClick={login}>Login</button>
+      <h2>Welcome to Coligo <small>({isLoggedIn ? 'Logged In' : 'Logged Out'})</small></h2>
     </div>
   </div>
 );
 
 Home.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
   login: PropTypes.func.isRequired,
 };
 
