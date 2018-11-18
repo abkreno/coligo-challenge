e3f80e439c3c16de8d68aadeb07b38faa8a67f39
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-18 22:18:02 +0200

    Add heroku scripts/ express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(`${__dirname}/public`));
+app.use('/assets', express.static(`${__dirname}/public/assets`));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+app.listen(port);
